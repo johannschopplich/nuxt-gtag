0c5ffcc45e0ebc5bebe4092bb77a68612c249536
Johann Schopplich <mail@johannschopplich.com>
2023-08-14 18:54:02 +0200

    feat: always return Gtag instance

--- src/runtime/composables/useGtag.ts
+++ src/runtime/composables/useGtag.ts
@@ -1,13 +1,9 @@
 import { gtag } from '../gtag'
 import type { Gtag } from '../types'
-import { useRuntimeConfig } from '#imports'
 
 export function useGtag(): Gtag {
-  const { gtag: { id } } = useRuntimeConfig().public
-
   // Return a noop function if this composable is called on the server
-  // or if there is no Gtag ID configured
-  if (!id || process.server)
+  if (process.server)
     return () => {}
 
   return gtag
