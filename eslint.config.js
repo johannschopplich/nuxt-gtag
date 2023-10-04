83b3e22dbbe60b3f5cbb262374ab89d6c5a5e504
Johann Schopplich <mail@johannschopplich.com>
2023-10-04 07:40:02 +0200

    chore: use flat ESLint config

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    rules: {
+      'node/prefer-global/process': 'off',
+    },
+  },
+)
--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,6 +1,6 @@
 declare global {
   interface Window {
-    dataLayer?: any[];
+    dataLayer?: any[]
   }
 }
 
