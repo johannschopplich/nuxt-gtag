b7af861bf708add91dc9d41f3a77d9aac3ac2d0b
Johann Schopplich <mail@johannschopplich.com>
2024-01-01 11:59:39 +0100

    perf: load client plugin in parallel

--- src/runtime/plugin.client.ts
+++ src/runtime/plugin.client.ts
@@ -2,26 +2,29 @@ import type { ModuleOptions } from '../module'
 import { initGtag } from './gtag'
 import { defineNuxtPlugin, useHead, useRuntimeConfig } from '#imports'
 
-export default defineNuxtPlugin(() => {
-  const { id, config, initialConsent, loadingStrategy } = useRuntimeConfig().public.gtag as Required<ModuleOptions>
+export default defineNuxtPlugin({
+  parallel: true,
+  setup() {
+    const { id, config, initialConsent, loadingStrategy } = useRuntimeConfig().public.gtag as Required<ModuleOptions>
 
-  if (!id)
-    return
+    if (!id)
+      return
 
-  initGtag({ id, config })
+    initGtag({ id, config })
 
-  if (!initialConsent)
-    return
+    if (!initialConsent)
+      return
 
-  // Sanitize loading strategy to be either `async` or `defer`
-  const strategy = loadingStrategy === 'async' ? 'async' : 'defer'
+    // Sanitize loading strategy to be either `async` or `defer`
+    const strategy = loadingStrategy === 'async' ? 'async' : 'defer'
 
-  useHead({
-    script: [
-      {
-        src: `https://www.googletagmanager.com/gtag/js?id=${id}`,
-        [strategy]: true,
-      },
-    ],
-  })
+    useHead({
+      script: [
+        {
+          src: `https://www.googletagmanager.com/gtag/js?id=${id}`,
+          [strategy]: true,
+        },
+      ],
+    })
+  },
 })
