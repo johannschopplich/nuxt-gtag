0e4bb03945c5fef431c3c694138bd218fb8f1756
Johann Schopplich <mail@johannschopplich.com>
2024-02-28 14:15:34 +0100

    perf: resolve options raw once

--- src/runtime/utils.ts
+++ src/runtime/utils.ts
@@ -3,12 +3,14 @@ import type { GoogleTagOptions } from './types'
 import { toRaw } from '#imports'
 
 export function resolveTags(options: Required<ModuleOptions>) {
+  const _options = toRaw(options)
+
   // Normalize tags
-  const tags: GoogleTagOptions[] = toRaw(options).tags.filter(Boolean)
+  const tags: GoogleTagOptions[] = _options.tags.filter(Boolean)
     .map(i => typeof i === 'string' ? { id: i } : i)
 
   if (options.id) {
-    const { id, config, initCommands } = toRaw(options)
+    const { id, config, initCommands } = _options
     tags.unshift({ id, config, initCommands })
   }
 
