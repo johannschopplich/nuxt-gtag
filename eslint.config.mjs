909a2057de07afc04fa16ef81cffe57445d040e4
Johann Schopplich <mail@johannschopplich.com>
2025-06-04 19:54:25 +0200

    chore: lint with `@antfu/eslint-config`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,41 +1,6 @@
-import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
-import perfectionist from 'eslint-plugin-perfectionist'
+// @ts-check
+import antfu from '@antfu/eslint-config'
 
-export default createConfigForNuxt({
-  features: {
-    tooling: true,
-    stylistic: true,
-  },
-  dirs: {
-    src: [
-      './playground',
-    ],
-  },
+export default antfu({
+  vue: true,
 })
-  .append({
-    plugins: {
-      perfectionist,
-    },
-    rules: {
-      'import/order': 'off',
-      'perfectionist/sort-exports': ['error', { order: 'asc', type: 'natural' }],
-      'perfectionist/sort-imports': ['error', {
-        groups: [
-          'type',
-          ['parent-type', 'sibling-type', 'index-type'],
-          'builtin',
-          'external',
-          ['internal', 'internal-type'],
-          ['parent', 'sibling', 'index'],
-          'side-effect',
-          'object',
-          'unknown',
-        ],
-        newlinesBetween: 'ignore',
-        order: 'asc',
-        type: 'natural',
-      }],
-      'perfectionist/sort-named-exports': ['error', { order: 'asc', type: 'natural' }],
-      'perfectionist/sort-named-imports': ['error', { order: 'asc', type: 'natural' }],
-    },
-  })
